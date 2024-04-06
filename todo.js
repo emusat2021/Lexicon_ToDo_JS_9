8c20ad876841d0a38327b7c20bceaa830d98aae4
Eugenia <musat.eugenia@gmail.com>
2024-04-06 14:45:04 +0200

    first commit

---
+++ todo.js
@@ -0,0 +1,14 @@
+console.log('hej');
+
+const addItemForm =  document.querySelector('#addForm');
+const output = document.querySelector('#output');
+
+let itemList = function(item){
+    let toDoList = {};
+    toDoList.add(item);
+    return toDoList;
+};
+
+addItemForm.addEventListener('sumbit', (e)=>{
+
+});
