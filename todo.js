8765181b29c3acfbd287a8615c89a9d5ea6a0b19
Eugenia <musat.eugenia@gmail.com>
2024-04-07 12:34:24 +0200

    Add item to the list and display it

--- todo.js
+++ todo.js
@@ -2,13 +2,37 @@ console.log('hej');
 
 const addItemForm =  document.querySelector('#addForm');
 const output = document.querySelector('#output');
-
-let itemList = function(item){
-    let toDoList = {};
-    toDoList.add(item);
-    return toDoList;
+let itemList = [];
+function addItem(item){
+    itemList.push(item);
 };
 
-addItemForm.addEventListener('sumbit', (e)=>{
+addItemForm.addEventListener('submit', (e)=>
+{
+    e.preventDefault();
+    //add input to the list of items
+    const userInput = document.getElementById('addItem').value;
+    addItem(userInput);
 
+    // clean content output after every input added
+    output.innerHTML = '';
+    // display every input/item by itereating the list
+    for(let i = 0; i < itemList.length; i++)
+    {
+        showMessage(`${i + 1} ${itemList[i]}`);
+    }
+    e.target.reset();
 });
+
+// output.addEventListener('doneBtn', (e)=>{
+
+// });
+
+// output.addEventListener('deleteBtn', (e)=>{
+
+// });
+
+function showMessage(message)
+{
+    output.innerHTML += message + "<br>";
+}
