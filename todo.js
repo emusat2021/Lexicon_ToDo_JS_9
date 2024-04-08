557551491337ebd1ac48fab2460919b056e8ed97
Eugenia <musat.eugenia@gmail.com>
2024-04-08 16:56:35 +0200

    Add functionality to ToDo

--- todo.js
+++ todo.js
@@ -3,36 +3,87 @@ console.log('hej');
 const addItemForm =  document.querySelector('#addForm');
 const output = document.querySelector('#output');
 let itemList = [];
-function addItem(item){
-    itemList.push(item);
+
+function addItem(item)
+{
+    itemList.push({text: item, done: false});
+    displayItems();
 };
 
+// capture user input/item
 addItemForm.addEventListener('submit', (e)=>
 {
     e.preventDefault();
-    //add input to the list of items
-    const userInput = document.getElementById('addItem').value;
+    //add user input as a item to the list items
+    const userInput = document.getElementById('userInput').value;
     addItem(userInput);
-
-    // clean content output after every input added
-    output.innerHTML = '';
-    // display every input/item by itereating the list
-    for(let i = 0; i < itemList.length; i++)
-    {
-        showMessage(`${i + 1} ${itemList[i]}`);
-    }
     e.target.reset();
 });
 
-// output.addEventListener('doneBtn', (e)=>{
-
-// });
-
-// output.addEventListener('deleteBtn', (e)=>{
-
-// });
-
-function showMessage(message)
+// logic to create and display the list of items in document
+function displayItems()
 {
-    output.innerHTML += message + "<br>";
+    output.innerHTML = '';
+
+    for(let i = 0; i < itemList.length; i++)
+    {
+        //ADD CONTAINER
+        const itemContainer = document.createElement('div');
+        itemContainer.classList.add('container');
+        //ADD ITEM
+        const itemText = document.createElement('span');
+        itemText.textContent = `${i+1}. ${itemList[i].text}`;
+        if(itemList[i].done)
+        {
+            itemText.classList.add('change');
+        }
+        itemContainer.appendChild(itemText);
+        //DONE BTN
+        const btnDone = document.createElement('button');
+        btnDone.innerText = 'Done';
+        btnDone.classList.add('btn');
+        if(itemList[i].done)
+        {
+            btnDone.classList.remove('btn-success');
+        }
+        else
+        {
+            btnDone.classList.add('btn-success');
+        }
+        btnDone.addEventListener('click', () => toggleDone(i));
+        itemContainer.appendChild(btnDone);
+        //UNDONE BTN
+        const btnUndone = document.createElement('button');
+        btnUndone.innerText = 'Undone';
+        btnUndone.classList.add('btn', 'btn-warning');
+        btnUndone.addEventListener('click', () => toggleUndone(i));
+        itemContainer.appendChild(btnUndone);
+        //DELETE BTN
+        const btnDelete = document.createElement('button');
+        btnDelete.innerText = 'Delete';
+        btnDelete.classList.add('btn', 'btn-danger');
+        btnDelete.addEventListener('click', () => deleteItem(i));
+        itemContainer.appendChild(btnDelete);
+
+        output.appendChild(itemContainer);
+    }
+
+};
+
+function toggleDone(i)
+{
+    itemList[i].done = true;
+    displayItems();
+}
+
+function toggleUndone(i)
+{
+    itemList[i].done = false;
+    displayItems();
+}
+
+function deleteItem(i)
+{
+    itemList.splice(i, 1);
+    displayItems();
 }
